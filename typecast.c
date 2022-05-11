73179a5d02bbe87d3cb1b085bd2e77b5d4930ece
sindhu <sindhushree.jm@thundersoft.com>
2022-05-11 20:00:27 +0530

    added files and typecast programs

---
+++ typecast.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void swap(void *a,void *b)
+{
+	*(int *)(a)=*(int *)(a)+(*(int *)(b));
+	*(int *)(b)=*(int *)(a)-(*(int *)(b));
+	*(int *)(a)=*(int *)(a)-(*(int *)(b));
+}
+int main()
+{
+	int a,b;
+	printf("Enter values of a and b: \n");
+	scanf("%d %d",&a,&b);
+	printf("Before swapping a = %d b = %d\n",a,b);
+	swap(&a,&b);
+	printf("After swapping a = %d b = %d\n",a,b);
+}
